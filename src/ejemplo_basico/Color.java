fc068f6e79e8b5df0d0cd5dbd53d16d9c4ea1900
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2023-05-29 22:57:15 -0500

    Implemente la Implementación: Color.

---
+++ src/ejemplo_basico/Color.java
@@ -0,0 +1,11 @@
+package ejemplo_basico;
+
+/**
+ * Esta es la interfaz que define la interfaz para las IMPLEMENTACIONES de colores.
+ */
+public interface Color {
+    /**
+     * Rellena la forma con el color.
+     */
+    void rellenar();
+}	
